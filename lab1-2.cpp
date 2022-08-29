c6bc619595f4f83907a6afdce96654f7de128e8a
ggmanuel1998 <51257596+ggmanuel1998@users.noreply.github.com>
2022-08-29 16:05:58 -0500

    Add files via upload

---
+++ lab1-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define MAX 20000
+#include <ctime>
+#include <conio.h>
+#include <time.h>
+using namespace std;
+
+unsigned t0, t1;
+long A[MAX][MAX], x[MAX], y[MAX];
+
+int main()
+{
+  t0=clock();
+  int i,j;
+  
+  for (int p = 0; p < MAX; p++){
+	for (int o = 0; o < MAX; o++){
+		A[o][p]=rand();
+	}
+  }
+
+  for (j = 0; j < MAX; j++){
+	for (i = 0; i < MAX; i++){
+		y[i] += A[i][j]*x[j];
+		t1 = clock();
+	}
+  }
+  double time = (double(t1-t0)/CLOCKS_PER_SEC);
+  cout << "Tiempo de ejecucion: " << time << endl;
+}
---
+++ lab1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define MAX 20000
+#include <ctime>
+using namespace std;
+
+unsigned t0, t1;
+long A[MAX][MAX], x[MAX], y[MAX];
+
+int main()
+{
+  t0=clock();
+  int i,j;
+  for (int p = 0; p < MAX; p++){
+	  for (int o = 0; o < MAX; o++){
+		  A[o][p]=rand();
+	  }
+  }
+    for (i = 0; i < MAX; i++)
+      for (j = 0; j < MAX; j++)
+        y[i] += A[i][j]*x[j];
+  t1 = clock();
+  
+  double time = (double(t1-t0)/CLOCKS_PER_SEC);
+  cout << "Tiempo de ejecucion: " << time << endl;
+}
